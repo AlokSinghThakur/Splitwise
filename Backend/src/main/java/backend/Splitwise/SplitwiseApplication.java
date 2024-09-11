b8ad0587824d49990aa7f8b55d814efdbf19aa75
pranavikadatla <datlapranavika205@gmail.com>
2024-09-11 08:30:06 +0530

    added backend with dependencies

---
+++ Backend/src/main/java/backend/Splitwise/SplitwiseApplication.java
@@ -0,0 +1,13 @@
+package backend.Splitwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseApplication.class, args);
+	}
+
+}
