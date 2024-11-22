8edeb19d29c62f333cd69875a1b7b0633b956106
Alok Singh Thakur <56357238+AlokSinghThakur@users.noreply.github.com>
2024-11-22 10:23:55 +0530

    added new files

--- splitwise-frontend/src/main.js
+++ splitwise-frontend/src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './tailwind.css'; // Import Tailwind
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
